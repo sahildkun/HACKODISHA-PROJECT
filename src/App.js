9b14886007e7ebf581b16c7c209db2e711b837de
Darkdev112 <devashritcode@gmail.com>
2022-09-11 11:32:46 +0530

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 import Navbar from './Navbar';
 function App() {
   return (
-    <div className="App">
-      <Navbar></Navbar>
-    </div>
+    <BrowserRouter>
+      <>
+        <Navbar></Navbar>
+        <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About />}/>
+        </Routes>
+      </>
+    </BrowserRouter>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,20 +1,34 @@
 import { useAuth0 } from "@auth0/auth0-react";
-
+import { useEffect } from "react";
+import {Link,useLocation} from "react-router-dom";
 
 const Navbar = () => {
 
   const { loginWithRedirect } = useAuth0();
-
-    return ( 
-
-      <nav className="navbar">
-         <h1>PROJECTNAME</h1>
-      <div className="links">
-        <a href="#"> login</a>
-         <button onClick={() => loginWithRedirect()}>Log In</button>
+  let location = useLocation();
+  useEffect(() => {
+  }, [location])
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="#">Navbar</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname==="/"?"active":""}`} aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname==="/about"?"active":""}`} to="/about">About</Link>
+            </li>
+          </ul>
+          <button className="btn btn-primary links" onClick={()=>loginWithRedirect()} aria-disabled="true">Login</button>
+        </div>
       </div>
-   </nav>
-     );
+    </nav>
+  );
 }
- 
-export default  Navbar ;
+
+export default Navbar;
